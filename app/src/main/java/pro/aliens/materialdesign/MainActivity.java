7043256058ccead79d12d31719301e74b29ab741
kilowat <kadantsev.sergey@gmail.com>
2016-05-08 16:57:22 +0400

    add toolbar

---
+++ app/src/main/java/pro/aliens/materialdesign/MainActivity.java
@@ -0,0 +1,38 @@
+package pro.aliens.materialdesign;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.navigat:
+                startActivity(new Intent(this,SubActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
